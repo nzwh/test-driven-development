a450da33a0141f351e3b949e95c3224b42d6fe49
jaye <74174900+shiopao@users.noreply.github.com>
2024-02-07 18:23:46 +0800

    Add failing test for deletePost

--- node-unit-test/controllers/post.test.js
+++ node-unit-test/controllers/post.test.js
@@ -1,19 +1,15 @@
 const postController = require ("./postController");
 // const { jest } = require ("@jest/globals");
 
-describe('getUserPosts', () => {
-  it('should retrieve user posts correctly', async () => {
-    const userId = 'user';
-    const callback = jest.fn(); // Using jest.fn() directly
+describe('deletePost', () => {
+  it('should delete a post correctly', async () => {
+    const postId = '1';
+    const callback = jest.fn();
 
-    const mockPosts = [
-      { _id: '1', title: 'Post 1', content: 'Content 1' },
-      { _id: '2', title: 'Post 2', content: 'Content 2' }
-    ];
-
-    postController.postModel.getByUser = jest.fn().mockResolvedValue(mockPosts);
-    postController.getUserPosts(userId, callback);
-    expect(postController.postModel.getByUser).toHaveBeenCalledWith(userId, expect.any(Function));
-    expect(callback).toHaveBeenCalledWith(mockPosts);
+    postController.postModel.deletePost = jest.fn().mockResolvedValue();
+    await postController.deletePost(postId, callback);
+    
+    expect(postController.postModel.deletePost).toHaveBeenCalledWith(postId, expect.any(Function));
+    expect(callback).toHaveBeenCalled();
   });
 });
