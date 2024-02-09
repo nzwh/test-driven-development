21c7f221175c953140d031f41bb493ea862ff9c8
jaye <74174900+shiopao@users.noreply.github.com>
2024-02-09 22:48:07 +0800

    Add failing test for getting all posts

--- node-unit-mvc/tests/post.test.js
+++ node-unit-mvc/tests/post.test.js
@@ -201,4 +201,130 @@ describe('Post controller', () => {
             sinon.assert.calledOnce(res.status(500).end);
         });
     })
+
+        // test case: find post
+    describe('findPost', () => {
+        
+        // default variables
+        var findPostStub;
+      
+        // executed before every test case
+        beforeEach(() => {
+            res = {
+                json: sinon.spy(),
+                status: sinon.stub().returns({ end: sinon.spy() })
+            };
+        });
+        // executed after every test case
+        afterEach(() => {
+            findPostStub.restore();
+        });
+
+        // intended behavior
+        it('should return the found post object', () => {
+
+            // model request
+            req = {
+                body: {
+                    title: 'First test post',
+                }
+            };
+            // arrange
+            expectedResult = {
+                _id: '200200',
+                title: 'First test post',
+                content: 'Random content',
+                author: 'stswenguser',
+                date: Date.now()
+            };
+
+            // stub
+            findPostStub = sinon.stub(PostModel, 'findPost').yields(null, expectedResult);
+            // act
+            PostController.findPost(req, res);
+            // assert
+            sinon.assert.calledWith(PostModel.findPost, req.body);
+            sinon.assert.calledWith(res.json, sinon.match({ title: req.body.title }));
+        });
+
+        // unintended behavior (error scenario)
+        it('should return status 500 on server error', () => {
+            // arrange
+            findPostStub = sinon.stub(PostModel, 'findPost').yields(error);
+            // act
+            PostController.findPost(req, res);
+            // assert
+            sinon.assert.calledWith(PostModel.findPost, req.body);
+            sinon.assert.calledWith(res.status, 500);
+            sinon.assert.calledOnce(res.status(500).end);
+        });
+    })
+      
+    // test case: get all posts
+    describe('getAllPosts', () => {
+        
+        // default variables
+        var getAllPostsStub;
+      
+        // executed before every test case
+        beforeEach(() => {
+            res = {
+                json: sinon.spy(),
+                status: sinon.stub().returns({ end: sinon.spy() })
+            };
+        });
+        // executed after every test case
+        afterEach(() => {
+            getAllPostsStub.restore();
+        });
+
+        // intended behavior
+        it('should return all post objects', () => {
+
+            // arrange
+            expectedResult = [
+                {
+                    _id: '200200',
+                    title: 'First test post',
+                    content: 'Random content',
+                    author: 'stswenguser',
+                    date: Date.now()
+                },
+                {
+                  _id: '200201',
+                  title: 'Second test post',
+                  content: 'Random content',
+                  author: 'stswenguser',
+                  date: Date.now()
+                },
+                {
+                  _id: '200202',
+                  title: 'Third test post',
+                  content: 'Random content',
+                  author: 'stswenguser',
+                  date: Date.now()
+                }
+            ];
+
+            // stub
+            getAllPostsStub = sinon.stub(PostModel, 'getAllPosts').yields(null, expectedResult);
+            // act
+            PostController.getAllPosts(req, res);
+            // assert
+            sinon.assert.calledWith(PostModel.getAllPosts, req.body);
+            sinon.assert.calledWith(res.json, sinon.match.array.deepEquals(expectedResult));
+        });
+
+        // unintended behavior (error scenario)
+        it('should return status 500 on server error', () => {
+            // arrange
+            getAllPostsStub = sinon.stub(PostModel, 'getAllPosts').yields(error);
+            // act
+            PostController.getAllPosts(req, res);
+            // assert
+            sinon.assert.calledWith(PostModel.getAllPosts, req.body);
+            sinon.assert.calledWith(res.status, 500);
+            sinon.assert.calledOnce(res.status(500).end);
+        });
+    })
 });
